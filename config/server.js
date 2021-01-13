69f6adc2fabb2e4b3b7d221b66df80d86d24ee2e
Tyler Candee <tcandee@mhelpdesk.com>
2021-01-12 20:48:59 -0500

    added strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '070ce54ab188522b4b484a647d330e22'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '673e4ce4-f928-4f94-9557-f4108b5ec6c4'
+};

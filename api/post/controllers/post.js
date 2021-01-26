5b323a258ccdcd79ab611b0737c9373bf9fc38d3
Tyler Candee <tyler@candeegenerations.com>
2021-01-26 09:14:57 -0500

    feat: moved to multiple tags

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const {sanitizeEntity} = require('strapi-utils')
+const {Types} = require('mongoose')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
@@ -11,6 +12,26 @@ module.exports = {
   async nextPosts(ctx) {
     const {id} = ctx.params
     const entities = await strapi.services.post.nextPosts(id)
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, {model: strapi.models.post}),
+    )
+  },
+
+  async copyTags() {
+    const entities = await strapi.services.post.find()
+
+    for (const index in entities) {
+      const post = entities[index]
+
+      if (!post.tags) {
+        post.tags = []
+      }
+
+      post.tags.push(Types.ObjectId(post.tag))
+
+      await strapi.services.post.update({id: post.id}, post)
+    }
 
     return entities.map((entity) =>
       sanitizeEntity(entity, {model: strapi.models.post}),
